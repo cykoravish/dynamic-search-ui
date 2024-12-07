13e17534b9e598685e86154624d52f4caaae7f08
Ravish Bisht <ravishbisht85@gmail.com>
2024-12-07 15:48:13 +0530

    dubug

--- src/api/dataFetcher.js
+++ src/api/dataFetcher.js
@@ -2,7 +2,7 @@ import { NEWS_API_KEY, MOVIE_API_KEY } from "./config.js";
 
 const fetchNews = async () => {
   const response = await fetch(
-    `https://newsapi.org/v2/top-headlines?country=us&apiKey=${NEWS_API_KEY}`
+    `https://newsapi.org/v2/top-headlines?country=us&apiKey=1c34ea2dfe2c468992eac7ccbca1450d`
   );
   const data = await response.json();
   return data.articles.map((article) => ({
@@ -33,7 +33,7 @@ const fetchBooks = async () => {
 
 const fetchMovies = async () => {
   const response = await fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_API_KEY}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=0d01f63e1aae052211ea93f252a8b015`
   );
   const data = await response.json();
   return data.results.map((movie) => ({
